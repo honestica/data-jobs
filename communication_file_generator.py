44197e410f1ea11889a384e02545f6006accf11e
HNassim <nassim.hmidou@lifen.fr>
2021-11-23 17:45:35 +0100

    clean communication file generator

--- communication_file_generator.py
+++ communication_file_generator.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
+"""
+Script used to generate 1000 random communication.
+"""
 import os
 import uuid
+
 from lib.communication_generator import sample_communication
 
 for _ in range(1000):
-	id = uuid.uuid4()
+    communication_id = uuid.uuid4()
 
-	filename = f'./communications/communication-{id}.json'
-	os.makedirs(os.path.dirname(filename), exist_ok=True)
+    filename = f"./communications/communication-{communication_id}.json"
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
 
-	with open(filename, 'w') as file:
-		com = sample_communication(id)
-		file.write(sample_communication(id))
-
-
-
+    with open(filename, "w") as file:
+        com = sample_communication(communication_id)
+        file.write(sample_communication(communication_id))
