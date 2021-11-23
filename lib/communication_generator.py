3ee1676786701a78a497eb9d4ff2318072004ab1
HNassim <nassim.hmidou@lifen.fr>
2021-11-23 17:39:50 +0100

    delete level 2 and clean functions

--- lib/communication_generator.py
+++ lib/communication_generator.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+"""
+All functions used to generate random communications.
+"""
 import random
 from datetime import datetime, timedelta
 
-START = datetime.strptime('2019-07-01 00:00:00', '%Y-%m-%d %H:%M:%S')
-END = datetime.strptime('2019-08-01 00:00:00', '%Y-%m-%d %H:%M:%S')
+START = datetime.strptime("2019-07-01 00:00:00", "%Y-%m-%d %H:%M:%S")
+END = datetime.strptime("2019-08-01 00:00:00", "%Y-%m-%d %H:%M:%S")
 
 
 def random_date(start, end):
     """
-    This function will return a random datetime between two datetime 
-    objects.
+    This function return a random datetime between two datetime objects.
     """
     delta = end - start
     int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
@@ -18,22 +20,26 @@ def random_date(start, end):
 
 
 def sample_communication(uuid):
-  telecoms = random.choice(["paper", "mail", "lifen"])
+    """
+    This function return a random communication.
+    """
+    telecoms = random.choice(["paper", "mail", "lifen"])
 
-  senders = random.choice(
-    [
-      {"name": "Jean Martin", "profession": "liberal"},
-      {"name": "Michel Durant", "profession": "public_hospital"},
-      {"name": "Herve Delatour", "profession": "liberal"},
-      {"name": "Maurice Rolland", "profession": "liberal"},
-      {"name": "Vincent Martinez", "profession": "private_hospital"}
-    ]
-  )
+    senders = random.choice(
+        [
+            {"name": "Jean Martin", "profession": "liberal"},
+            {"name": "Michel Durant", "profession": "public_hospital"},
+            {"name": "Herve Delatour", "profession": "liberal"},
+            {"name": "Maurice Rolland", "profession": "liberal"},
+            {"name": "Vincent Martinez", "profession": "private_hospital"},
+        ]
+    )
 
-  return "|".join([
-    f'id={uuid}',
-    f'telecom={telecoms}',
-    f'created_at={random_date(START, END).strftime("%Y-%m-%d %H:%M:%S")}',
-    f'sender={senders}'
-  ])
-  
+    return "|".join(
+        [
+            f"id={uuid}",
+            f"telecom={telecoms}",
+            f'created_at={random_date(START, END).strftime("%Y-%m-%d %H:%M:%S")}',
+            f"sender={senders}",
+        ]
+    )
